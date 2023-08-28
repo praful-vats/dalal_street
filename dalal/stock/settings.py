24b6980fdd501757fbc91592acf58205e7103d2d
Praful Vats <prafulvats01@gmail.com>
2023-08-28 13:12:24 +0530

    .ns

--- dalal/stock/settings.py
+++ dalal/stock/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('key')
 # print("SECRET_KEY:", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
